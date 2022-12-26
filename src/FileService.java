e9203dfff500921b00c9816c02afc607e63861f1
Bolot Ashirkulov <ashirkulovbolott@gmail.com>
2022-12-26 21:28:39 +0600

    changed Main.java to Main.java and added file Movies.java

--- src/FileService.java
+++ src/FileService.java
@@ -1,25 +1,26 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 
 public class FileService {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final Path PATH = Paths.get("data/database.json");
+    private static final Path PATH = Paths.get("./movies.json");
 
-    public static AccessToDatabase[] readFile() {
+    public static Map<String, List<Movie>> readFile() {
         String json = "";
         try {
             json = Files.readString(PATH);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("\nFile not found " + e.getMessage());
         }
-        return GSON.fromJson(json, AccessToDatabase[].class);
+        return GSON.fromJson(json, new TypeToken<Map<String, List<Movie>>>() {
+        }.getType());
     }
 }
-
-
-
